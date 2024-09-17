72fb34717c9ee2e63a106a877cd27f28e8bca7f7
Tyler Kenna <tyler.kenna@gmail.com>
2024-09-17 15:32:37 -0700

    Added a single test for AttributeStringEqual.

--- tests/OddDotNet.Aspire.Tests/SpanQueryServiceTests.cs
+++ tests/OddDotNet.Aspire.Tests/SpanQueryServiceTests.cs
@@ -59,4 +59,56 @@ public class SpanQueryServiceTests
             Assert.Equal(request.ResourceSpans[0].ScopeSpans[0].Spans[0].SpanId, response.Spans[0].SpanId);
         }
     }
+
+    public class WhereAttributeStringEqualShould
+    {
+        [Fact]
+        public async Task ReturnSpanWithMatchingAttributeWhenAttributeIsEqual()
+        {
+            var request = TestHelpers.CreateExportTraceServiceRequest();
+
+            var builder = await DistributedApplicationTestingBuilder.CreateAsync<Projects.OddDotNet_Aspire_AppHost>();
+            await using var app = await builder.BuildAsync();
+            
+            var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
+            await app.StartAsync();
+
+            await resourceNotificationService.WaitForResourceAsync("odd").WaitAsync(TimeSpan.FromSeconds(30));
+
+            var endpoint = app.GetEndpoint("odd", "http");
+            var traceServiceChannel = GrpcChannel.ForAddress(endpoint.AbsoluteUri);
+            TraceService.TraceServiceClient traceServiceClient = new TraceService.TraceServiceClient(traceServiceChannel);
+
+            await traceServiceClient.ExportAsync(request);
+            
+            var spanQueryChannel = GrpcChannel.ForAddress(endpoint.AbsoluteUri);
+            SpanQueryService.SpanQueryServiceClient spanQueryServiceClient = new SpanQueryService.SpanQueryServiceClient(spanQueryChannel);
+            
+            var take = new Take
+            {
+                TakeExact = new TakeExact()
+                {
+                    Count = 1
+                }
+            };
+
+            var spanToFind = request.ResourceSpans[0].ScopeSpans[0].Spans[0];
+            var attributeToFind = spanToFind.Attributes[0];
+            var whereFilter = new Where
+            {
+                AttributeStringEqual = new WhereAttributeStringEqualFilter()
+                {
+                    Attribute = attributeToFind.Key,
+                    Compare = attributeToFind.Value.StringValue
+                }
+            };
+            
+            var spanQueryRequest = new SpanQueryRequest { Take = take, WhereFilters = { whereFilter }};
+            
+            var response = await spanQueryServiceClient.QueryAsync(spanQueryRequest);
+            
+            Assert.NotEmpty(response.Spans);
+            Assert.Equal(spanToFind.SpanId, response.Spans[0].SpanId);
+        }
+    }
 }
